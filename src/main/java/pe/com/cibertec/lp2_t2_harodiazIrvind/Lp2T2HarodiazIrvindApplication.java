adea2488f6a9102dd5461dae1bd607a0d903f895
Irvin D. Haro Diaz <i202334997@cibertec.edu.pe>
2024-10-13 20:10:46 -0500

    estructura inicial

---
+++ src/main/java/pe/com/cibertec/lp2_t2_harodiazIrvind/Lp2T2HarodiazIrvindApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.lp2_t2_harodiazIrvind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2HarodiazIrvindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2HarodiazIrvindApplication.class, args);
+	}
+
+}
